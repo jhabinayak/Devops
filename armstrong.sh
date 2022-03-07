c6fd875a093d2b8edfc38e0e1091ba8cf18fe694
jhabinayak <94355143+jhabinayak@users.noreply.github.com>
2022-03-07 14:01:02 +0530

    Update armstrong.sh

--- armstrong.sh
+++ armstrong.sh
@@ -12,4 +12,4 @@ if [ $arm -eq "$temp" ]; then
 else
 	echo "Not Armstrong"
 fi
-echo " Checking complete for amrstrong- not armstrong"
+echo " Checking complete wiht webhook for amrstrong- not armstrong"
